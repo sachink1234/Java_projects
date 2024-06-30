190e704f50ae8d64688fc18a744460c147c58179
sk2407 <sachinkumar24071999@gmail.com>
2024-06-30 16:55:10 +0530

    peoject setup

---
+++ src/main/java/com/example/jwttokenexample/JwttokenexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwttokenexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwttokenexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwttokenexampleApplication.class, args);
+	}
+
+}
